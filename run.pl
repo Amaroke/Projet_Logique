e7981ba7b127dd3419b8e3e8c8b58575fdee33b9
Amaroke <hugo.amaroke@gmail.com>
2022-11-02 09:44:37 +0100

    Initialisation du projet

---
+++ run.pl
@@ -0,0 +1,15 @@
+:- op(20,xfy,?=).
+
+% Prédicats d'affichage fournis
+
+% set_echo: ce prédicat active l'affichage par le prédicat echo
+set_echo :- assert(echo_on).
+
+% clr_echo: ce prédicat inhibe l'affichage par le prédicat echo
+clr_echo :- retractall(echo_on).
+
+% echo(T): si le flag echo_on est positionné, echo(T) affiche le terme T
+%          sinon, echo(T) réussit simplement en ne faisant rien.
+
+echo(T) :- echo_on, !, write(T).
+echo(_).
