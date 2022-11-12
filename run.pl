13a2c2947cfe26ebb2b19274a3d1c61c4d478946
Amaroke <hugo.amaroke@gmail.com>
2022-11-12 18:07:08 +0100

    Multiples corrections pour la question 1 et ajouts de tests

--- run.pl
+++ run.pl
@@ -1,5 +1,4 @@
-% Permet de retirer les Warnings liés au Singleton
-:- style_check(-singleton).
+:- dynamic echo_on/0. % On informe l'interpréteur que la définition du prédicat change durant l'execution (pour éviter un warning de l'éditeur)
 
 % Prédicats d'affichage fournis
 % set_echo: ce prédicat active l'affichage par le prédicat echo
@@ -17,56 +16,61 @@ echo(_).
 % Définition de l'opérateur ?=
 :- op(20, xfy, ?=).
 
+
 % Question 1
 % Définition des règles
 % Définiton de la règle Rename
-regle(X ?= T, rename) :- var(X), var(T), !.
+regle(X ?= T, rename) :- var(X), var(T).
 
 % Définition de la règle Simplify
-regle(X ?= T, simplify) :- var(X), atomic(T), !.
+regle(X ?= T, simplify) :- var(X), atomic(T).
 
 % Définition de la règle Expand
-regle(X ?= T, expand) :- compound(T), var(X), not(occur_check(X, T)), !.
+regle(X ?= T, expand) :- var(X), compound(T), \+occur_check(X, T).
 
 % Défintion de la règle Check
-regle(X ?= T, check) :- X \== T, var(X), occur_check(X, T), !. 
+regle(X ?= T, check) :- X \== T, var(X), compound(T), occur_check(X, T), !. 
 
 % Définition de la règle Orient
-regle(T ?= X, orient) :- nonvar(T), var(X), !.
+regle(T ?= X, orient) :- nonvar(T), var(X).
 
 % Définition de la règle Decompose
-regle(X ?= Y, decompose) :- compound(X), compound(Y), compound_name_arity(X, NAME1, ARITY1), compound_name_arity(Y, NAME2, ARITY2),  NAME1 == NAME2, ARITY1 == ARITY2, !.
+regle(X ?= Y, decompose) :- nonvar(X), nonvar(Y), functor(X, Name1, Arity1), functor(Y, Name2, Arity2),  Name1 == Name2, Arity1 == Arity2.
 
 % Définition de la règle Clash
-regle(X ?= Y, clash) :- compound(X), compound(Y), compound_name_arity(X, NAME1, ARITY1), compound_name_arity(Y, NAME2, ARITY2), not((NAME1 == NAME2, ARITY1 == ARITY2)), !.
-
+regle(X ?= Y, clash) :- nonvar(X), nonvar(Y), functor(X, Name1, Arity1), functor(Y, Name2, Arity2), (Name1 \== Name2; Arity1 \== Arity2), !.
 
 % Définition de occur_check
-occur_check(V, T) :- var(V), compound(T), arg(_, T, X), V == X, !.
+occur_check(V, T) :- var(V), compound(T), arg(_, T, X), (V == X ; (compound(X), occur_check(V, X))), !.
 
 
 % Définition des réductions
 % Définition de réduction de Rename
-reduit(rename, X ?= T, P, Q) :- X = T, Q = P, !.
+reduit(rename, X ?= T, P, Q) :- X = T, Q = P.
 
 % Définition de réduction de Simplify
-reduit(simplify, X ?= T, P, Q) :- X = T, Q = P, !.
+reduit(simplify, X ?= T, P, Q) :- X = T, Q = P.
 
 % Définition de réduction de Expand 
-reduit(expand, X ?= T, P, Q) :- X = T, Q = P, !.
+reduit(expand, X ?= T, P, Q) :- X = T, Q = P.
 
 % Définition de réduction de Check
-reduit(check, X ?= T, P, Q) :- fail, !.
+reduit(check, _, _, _) :- !, fail.
 
 % Définition de réduction de Orient
-reduit(orient, T ?= X, P, Q) :- append([X ?= T], P, Q), !.
+reduit(orient, T ?= X, P, [X ?= T | P]).    
 
 % Définition de réduction de Decompose
-reduit(decompose, X ?= Y, P, Q) :- compound_name_arguments(X, NAME1, ARGS1), compound_name_arguments(Y, NAME2, ARGS2), remplace(ARGS1, ARGS2, RESULT), append(RESULT, P, Q).
+reduit(decompose, X ?= Y, P, Q) :- compound_name_arguments(X, _, Args1), compound_name_arguments(Y, _, Args2), remplace(Args1, Args2, Result), append(Result, P, Q).
 
 % Définition de réduction de Clash
-reduit(clash, X ?= Y, P, Q) :- fail, !.
+reduit(clash, _, _, _) :- !, fail.
 
 % Définition de remplace qui pose récursivement l'unification de tous les termes un à un
 remplace([], [], []). % Condition d'arrêt si les paramètres sont vides (pour éviter une boucle infinie)
-remplace([A|ARGS1], [B|ARGS2], RESULT) :- remplace(ARGS1, ARGS2, TEMP), append([A ?= B], TEMP, RESULT).
+remplace([A|Args1], [B|Args2], [A ?= B | Temp]) :- remplace(Args1, Args2, Temp).
+
+
+% Définition de Unifie
+unifie([E|P]):- regle(E, R), reduit(R, E, P, Q), !, unifie(Q).
+unifie([]).
